11d624952c1f2c82d7d3515e5bfde1aa4801383d
mertkaya20 <mertkaya.codes@gmail.com>
2026-09-12 17:20:46 +0300

    chore: configure Knex for better-sqlite3 with migrations/seeds paths

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "better-sqlite3",
+    connection: {
+      filename: "./data/todo-api.sqlite3",
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+    useNullAsDefault: true,
+  },
+};
